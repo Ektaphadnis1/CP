29cf3de010f51a6db099ba45726a4e7fafb30933
Ektaphadnis1 <ektajoshi2008@gmail.com>
2026-03-22 15:09:16 +0530

    Implement word counting in (i).cpp
    Fix the word counting logic and output the count.

---
+++ EXP1/(i).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+int main(){
+    string s, word;
+    getline(cin,s);
+    stringstream ss(s);
+    int count=0;
+    while(ss>word){
+        count++;
+    }
+    cout<<cout;
+    return 0;
+}
