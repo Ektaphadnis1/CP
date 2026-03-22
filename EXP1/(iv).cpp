3848a3286c60ccf9c94be532667225d6de8f6f3a
Ektaphadnis1 <ektajoshi2008@gmail.com>
2026-03-22 15:12:23 +0530

    program to calculate exponential of m raised to n

---
+++ EXP1/(iv).cpp
@@ -0,0 +1,12 @@
+// To find exponential number (m,n)
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    double m, n;
+    cout<<"Enter the value of m and n: ";
+    cin>>m>>n;
+    double result = pow(m,n);
+    cout<<result<<endl;
+    return 0;
+}
